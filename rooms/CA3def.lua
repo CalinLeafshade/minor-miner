623f93c610257e1d17f839c7771d3dc25be2164d
Steven Poulton <poulton.steven@googlemail.com>
2013-07-05 18:25:37 +0100

    checkpoint

--- rooms/CA3def.lua
+++ rooms/CA3def.lua
@@ -2,8 +2,7 @@
 return {
   enemies={
     { Name="mushroom", Parameters={ maxX=240, minX=90 }, X=226.95, Y=161.5 },
-    { Name="bat", Parameters={ speed=20, wakeDistance=50 }, X=223, Y=25 },
-    { Name="runner", Parameters={ platform=1, speed=20 }, X=100.84, Y=95.032 } 
+    { Name="bat", Parameters={ speed=20, wakeDistance=50 }, X=223, Y=25 }
   },
   platforms={
     { mode="allblock", verts={ 111, 95, 111, 111, 1, 111, 1, 95 } },
--- rooms/CA4def.lua
+++ rooms/CA4def.lua
@@ -1,22 +1,22 @@
--- Generated At 02:34:35
+-- Generated At 03:31:30
 return {
   enemies={  },
   platforms={
-    { mode="allblock", verts={ 0, 0, 20, 0, 20, 92, 0, 92 } },
-    { mode="allblock", verts={ 20, 40, 60, 40, 60, 49, 20, 49 } },
-    { mode="allblock", verts={ 228, 8, 82, 8, 82, 0, 228, 0 } },
-    { mode="allblock", verts={ 129, 84, 83, 84, 83, 75, 129, 75 } },
-    { mode="allblock", verts={ 60, 140, 0, 140, 0, 130, 60, 130 } },
-    { mode="allblock", verts={ 129, 195, 84, 195, 84, 184, 129, 184 } },
-    { mode="allblock", verts={ 1, 239, 62, 239, 62, 250, 1, 250 } },
-    { mode="allblock", verts={ 129, 302, 85, 302, 85, 291, 129, 291 } },
-    { mode="allblock", verts={ 319, 359, 0, 359, 0, 347, 319, 347 } },
-    { mode="allblock", verts={ 319, 270, 298, 270, 298, 171, 319, 171 } },
-    { mode="allblock", verts={ 297, 231, 268, 231, 268, 218, 297, 218 } },
-    { mode="allblock", verts={ 254, 271, 219, 271, 219, 258, 254, 258 } },
-    { mode="allblock", verts={ 256, 185, 219, 185, 219, 174, 256, 174 } },
-    { mode="allblock", verts={ 130, 9, 218, 9, 218, 302, 130, 302 } },
-    { mode="allblock", verts={ 319, 115, 219, 115, 219, 89, 319, 89 } },
-    { mode="allblock", verts={ 20, 346, 0, 346, 0, 141, 20, 141 } } 
+    { mode="allblock", verts={ 0, 92, 0, 0, 20, 0, 20, 92 } },
+    { mode="allblock", verts={ 20, 49, 20, 40, 60, 40, 60, 49 } },
+    { mode="allblock", verts={ 228, 0, 228, 8, 82, 8, 82, 0 } },
+    { mode="allblock", verts={ 129, 75, 129, 84, 83, 84, 83, 75 } },
+    { mode="allblock", verts={ 60, 130, 60, 140, 0, 140, 0, 130 } },
+    { mode="allblock", verts={ 129, 184, 129, 195, 84, 195, 84, 184 } },
+    { mode="allblock", verts={ 1, 250, 1, 239, 62, 239, 62, 250 } },
+    { mode="allblock", verts={ 129, 291, 129, 302, 85, 302, 85, 291 } },
+    { mode="allblock", verts={ 319, 347, 319, 359, 0, 359, 0, 347 } },
+    { mode="allblock", verts={ 319, 171, 319, 270, 298, 270, 298, 171 } },
+    { mode="allblock", verts={ 297, 218, 297, 231, 268, 231, 268, 218 } },
+    { mode="allblock", verts={ 254, 258, 254, 271, 219, 271, 219, 258 } },
+    { mode="allblock", verts={ 256, 174, 256, 185, 219, 185, 219, 174 } },
+    { mode="allblock", verts={ 130, 302, 130, 9, 218, 9, 218, 302 } },
+    { mode="allblock", verts={ 319, 89, 319, 115, 219, 115, 219, 89 } },
+    { mode="allblock", verts={ 20, 141, 20, 346, 0, 346, 0, 141 } } 
   } 
 }
